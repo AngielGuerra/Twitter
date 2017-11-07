ede7a8842fa8ee1a4a77e4e39fda6fd1f3ddd3e8
AngielGuerra <ang.guerra.g@gmail.com>
2017-11-07 17:25:22 -0300

    Versión 0.0.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,38 @@
 function add(){
 	//almacenando valor//
-	var comments = document.getElementById("comment").value; //rescato el valor con . value//
-	//para limpiar el valor//
-	document.getElementById("comment").value = "";
-	var newComments = document.createElement('div');//crear un div que contenga todo lo que viene//
-    var cont = document.getElementById('cont');//agrega un id al contenedor div que se creo arriba//
-    var parrafo = document.createElement("p");
-    cont.classList.add("newdiv");
+	var comments = document.getElementById("tweet").value; //rescato el valor con . value//
+	document.getElementById("tweet").value = ""; //para limpiar el valor//
+    var boton = document.getElementById("btn");//rescato el boton en una variable
     var texto = document.createTextNode(comments);
+    var parrafo = document.createElement("p");
+    var newComments = document.createElement('div');//crear un div que contenga todo lo que viene//
+    cont.classList.add("newdiv");
     parrafo.appendChild(texto);
     newComments.appendChild(parrafo);
-    cont.appendChild(newComments); 
+    cont.appendChild(newComments);
 }
+btn.disabled=true;
+
+function limit(){
+    var tweet = document.getElementById("tweet").value;
+    var contador = document.getElementById("inp");
+    var boton = document.getElementById("btn")
+    var contadorMax = 140;
+    
+    contador.innerHTML = contadorMax - tweet.length; 
+
+    if(tweet.length > contadorMax){
+        btn.disabled = true;
+    }if(tweet.length < contadorMax){
+        btn.disabled = false;
+    }
+}
+
+
+
+
+
+
+
 
     
