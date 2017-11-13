202599aa95bbfa0ff93a108ef5ad005bbce8335d
AngielGuerra <ang.guerra.g@gmail.com>
2017-11-12 23:15:26 -0300

    version 00.3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 function add(){
-	//almacenando valor//
-	var comments = document.getElementById("tweet").value; //rescato el valor con . value//
-	document.getElementById("tweet").value = ""; //para limpiar el valor//
+    //almacenando valor//
+    var comments = document.getElementById("tweet").value; //rescato el valor con . value//
+    document.getElementById("tweet").value = ""; //para limpiar el valor//
+    limit();
     var boton = document.getElementById("btn");//rescato el boton en una variable
     var texto = document.createTextNode(comments);
     var parrafo = document.createElement("p");
@@ -11,20 +12,36 @@ function add(){
     newComments.appendChild(parrafo);
     cont.appendChild(newComments);
 }
-btn.disabled=true;
 
 function limit(){
     var tweet = document.getElementById("tweet").value;
     var contador = document.getElementById("inp");
     var boton = document.getElementById("btn")
     var contadorMax = 140;
-    
-    contador.innerHTML = contadorMax - tweet.length; 
+
+        colors();
+
+    contador.innerHTML = contadorMax - tweet.length;
 
     if(tweet.length > contadorMax){
         btn.disabled = true;
     }if(tweet.length < contadorMax){
         btn.disabled = false;
+    }
+}
+function colors(){
+    var caracteres = document.getElementById("tweet").value.length;
+    var contador = document.getElementById("inp");
+    var boton = document.getElementById("btn");
+    var contadorMax = 140;
+    contador.innerHTML = contadorMax - tweet.length;
+
+    if(caracteres >=120 && caracteres <= 130 ) {
+        contador.style.color="red";
+    }else if (caracteres >=131){
+        contador.style.color="blue";
+    }else {
+        (contador.style.color="black");
     }
 }
 
